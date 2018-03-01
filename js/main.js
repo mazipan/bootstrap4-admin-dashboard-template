d4b74f36bda847f6ec1e13a9f9130fad869af7a1
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2018-03-01 17:09:48 +0700

    add collapse nav

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
 $(document).ready(function() {
+  initMenuCollapsible();
   initCardCollapsible();
 });
 
-function initCardCollapsible() {
+function initCardCollapsible () {
   $('.caret').on('click', function (evt) {
-    $(this).toggleClass('fa-caret-up', 'fa-caret-down')
+    $(this).toggleClass('active')
     var cardHeader = $(this).parent();
     var cardBody = $(cardHeader).next();
-    $(cardBody[0]).toggle()
+    $(cardBody[0]).slideToggle()
+  })
+}
+
+function initMenuCollapsible () {
+  $('.nav-item.has-child').on('click', function (evt) {
+    var _self = $(this)
+    if (_self.hasClass('nav-item')) {
+      _self.children('a.nav-link').toggleClass('active')
+
+      if (_self.hasClass('has-child')) {
+        var nav = _self.find('.is-child')
+        $(nav).slideToggle()
+      }
+    }
   })
 }
